2294a079611debfc996f2396895c14072df2d467
apple2106 <apple2106@DESKTOP-01H3CGM>
2022-02-28 17:43:06 +0900

    2022-02-28 second: before task's not resolved.

--- start/src/day05/ex/ex0101.java
+++ start/src/day05/ex/ex0101.java
@@ -1,6 +1,6 @@
-package ex;
+package day05.ex;
 //1부터 10까지의 합을 구해서 출력하는 프로그램 작성
-public class ex01 {
+public class ex0101 {
 	public static void main(String[] args) {
 		int sum = 0;//결과값으로 사용할 변수 생성
 		for(int i = 0 ; i <= 10 ; i++) {   //반복문 작성 i는 0부터 시작 9까지 
---
+++ start/src/day05/ex/ex02.java
@@ -0,0 +1,14 @@
+package day05.ex;
+
+/*
+	Ex02 ]
+		'A' 부터 문자를 10개를 만들어서 출력하세요.
+		**선생님이 작성하신 클래스 복붙자료임 
+ */
+public class ex02 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
