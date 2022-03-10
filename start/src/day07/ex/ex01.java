18c6508c2edf776ea6e2c421758e11140eaf9e48
macbookpro <macbookpro@maegbugcbookpro>
2022-03-11 00:14:46 +0900

    2022-03-11.

--- start/src/day07/ex/ex01.java
+++ start/src/day07/ex/ex01.java
@@ -1,11 +1,26 @@
 package day07.ex;
 //'A'~'J'까지 문자를 랜덤하게 100개(문자열) 만들고 각 문자의 출현횟수를 기억할 배열을 만들어서 회수만큼 '*'로 출력해주는 프로그램
 public class ex01 {
+	
+	int[] chArry;
+	public ex01() {
+		String ch100 = randomChar();
+	}
 	public static void main(String[] args) {
-		char[] ch = new char[100];
+		new ex01();
+	}
+	public String randomChar() {
+		String ranch = "";
+		for(int i = 0 ; 0 < 100 ; i++) {			
+			ranch += ('A'+(Math.random()*10)+"");
+		}
+		return ranch;
+	}
+	public void setChar() {
+		chArry = new String[100][2];
 		for(int i = 0 ; i < 100 ; i++) {
-			
+			chArry[i][0] = ()+"";
+			chArry[i][1] = 1+"";
 		}
 	}
-
 }
