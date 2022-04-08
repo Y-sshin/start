43f7503652008c355b5cd64b6bfab49d0fdd622a
macbookpro <macbookpro@maegbugcbookpro>
2022-04-08 22:28:23 +0900

    debug create

---
+++ start/src/nothing/debug.java
@@ -0,0 +1,20 @@
+package nothing;
+
+public class debug {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		int j = 0;
+		
+		while(i<10) {
+			for(;j<10;j++) {
+				System.out.println("i는 "+i+" j는 "+j);
+			}
+			i++;
+		}
+	}
+}
+
+
